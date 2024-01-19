ae69a3ca795f20906fe4b3faa6d35897dd01cd83
Valentin Rieu <rieuvalentin3@gmail.com>
2024-01-19 23:09:55 +0100

    correction fichier compilation windows

--- compile.bat
+++ compile.bat
@@ -1,9 +1,15 @@
 dir /s /b "*.java" > classes.txt
-javac -d bin @classes.txt
+javac -d . @classes.txt
 del /f classes.txt
 
-dir /s /b "*.class" > classes.txt
-echo "Main-Class: jphoto.Main" > Manifest.txt
+setlocal disableDelayedExpansion
+for /f "delims=" %%A in ('forfiles /s /m *.class /c "cmd /c echo @relpath"') do (
+  set "file=%%~A"
+  setlocal enableDelayedExpansion
+  echo !file:~2! >> classes.txt
+  endlocal
+)
+echo Main-Class: jphoto.Main > Manifest.txt
 jar cfm JPhoto.jar Manifest.txt @classes.txt res
 
 del /f Manifest.txt
